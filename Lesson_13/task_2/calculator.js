441ba9d96390fafe66c7f8d6a6cf276150719f20
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-16 18:47:03 +0200

    FE-15

---
+++ Lesson_13/task_2/calculator.js
@@ -0,0 +1,11 @@
+// input: array
+// output: array
+export const getSquaredArray = arr => arr.map(el => el * el);
+
+// input: array
+// output: array
+export const getOddNumbers = arr => arr.filter(el => el % 2 === 1);
+
+// input: number
+// output: number
+export default (num1, num2) => num1 + num2;
