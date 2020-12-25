826d828e55616c59489d5d9f94ef6e36ca6d8450
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-25 12:04:37 +0200

    FE-15

---
+++ Lesson_21/HW_1/index.js
@@ -0,0 +1,14 @@
+const getTitleElement = () => {
+  const titleElement = document.querySelector('.title');
+
+  console.dir(titleElement);
+};
+
+const getInputElement = () => {
+  const inputElem = document.querySelector('input[type=text]');
+
+  console.dir(inputElem);
+};
+
+getTitleElement();
+getInputElement();
