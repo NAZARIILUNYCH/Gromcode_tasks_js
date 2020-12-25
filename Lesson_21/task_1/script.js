bd2d86fc07247275e4bf0819155caa461f9259ea
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-25 10:56:02 +0200

    FE-15

--- Lesson_21/task_1/script.js
+++ Lesson_21/task_1/script.js
@@ -9,13 +9,25 @@ const tasks = [
 const renderListItems = listItems => {
   const listElem = document.querySelector('.list');
 
-  const listItemsElems = listItems.map(itemText => {
-    const listItemElem = document.createElement('li');
-    listItemElem.classList.add('list__item');
-    listItemElem.append(itemText);
+  const listItemsElems = listItems
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
 
-    return listItemElem;
-  });
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      const checkboxElem = document.createElement('input');
+      checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.checked = done;
+
+      checkboxElem.classList.add('list__item-checkbox');
+      listItemElem.append(checkboxElem, text);
+
+      return listItemElem;
+    });
 
   listElem.append(...listItemsElems);
 };
