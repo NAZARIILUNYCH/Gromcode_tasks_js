8428a2d0ee6f59d2f638eed538d939ac7e7465d3
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-25 19:58:34 +0200

    FE-15

--- Lesson_21/HW_10/index.js
+++ Lesson_21/HW_10/index.js
@@ -2,7 +2,7 @@
 export const manageClasses = () => {
   const classOne = document.querySelector('.list > .one').classList.add('selected');
   const classTwo = document.querySelector('.list > .two').classList.remove('selected');
-  const classThree = document.querySelector('.list > .three').classList.toggle('three-done');
+  const classThree = document.querySelector('.list > .three').classList.toggle('three_done');
   const classFour = document.querySelector('.list > .some-class');
 
   classFour ? classFour.classList.add('another-class') : false;
