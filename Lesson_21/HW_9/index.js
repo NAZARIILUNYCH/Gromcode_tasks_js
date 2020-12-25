ad292a142b2c256b80be5f236ea4f724b716028f
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-25 19:09:04 +0200

    FE-15

--- Lesson_21/HW_9/index.js
+++ Lesson_21/HW_9/index.js
@@ -1,7 +1,7 @@
 export const finishForm = () => {
   const loginForm = document.querySelector('.login-form');
   const newForm = document.createElement('input');
-  const passwordForm = document.querySelector('input]');
+  const passwordForm = document.querySelector('input[name="password"]');
 
   newForm.setAttribute('type', 'text');
   newForm.setAttribute('name', 'login');
