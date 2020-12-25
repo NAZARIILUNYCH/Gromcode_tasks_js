ac68ee59af4a844a35ba337b93f7925ad5c7d026
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2020-12-25 19:50:55 +0200

    FE-15

---
+++ Lesson_21/HW_10/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+export const manageClasses = () => {
+  const classOne = document.querySelector('.list > .one').classList.add('selected');
+  const classTwo = document.querySelector('.list > .two').classList.remove('selected');
+  const classThree = document.querySelector('.list > .three').classList.toggle('three-done');
+  const classFour = document.querySelector('.list > .some-class');
+
+  classFour ? classFour.classList.add('another-class') : false;
+};
