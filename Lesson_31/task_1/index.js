0b3de43f2b7b7336e4ee7d3148f7027c113fb898
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-09 14:39:06 +0200

    FE-15

---
+++ Lesson_31/task_1/index.js
@@ -0,0 +1,26 @@
+const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId: `${userId}`,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+
+  return request;
+};
+
+requestUserData('broken')
+  .catch(error => {
+    console.log(error);
+  })
+  .then(data => console.log(data));
