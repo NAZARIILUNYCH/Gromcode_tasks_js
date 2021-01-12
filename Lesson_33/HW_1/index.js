7dce75079d235ac395449a228fa0eef75666d9b0
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-12 12:26:28 +0200

    FE-15

--- Lesson_33/HW_1/index.js
+++ Lesson_33/HW_1/index.js
@@ -1,18 +1,4 @@
-/* eslint-disable no-return-assign */
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
-const newArr = [];
 
-const getTasksList = url =>
-  fetch(`${url}`)
-    .then(task => task.json())
-    .then(taskElem =>
-      taskElem.forEach(element => {
-        newArr.push(element);
-      }),
-    );
-
-console.log(newArr);
-// const getTaskById = taskId => console.log(newArr);
-
-getTasksList(baseUrl);
-// getTaskById('10');
+export const getTasksList = url => fetch(`${baseUrl}`).then(task => task.json());
+export const getTaskById = taskId => fetch(`${baseUrl}/${taskId}`).then(taskId => taskId.json());
